80564ccfb43245c8a8195b2b54ed382fe001b228
SepehrMoalemi <sepehr.moalemi@mail.mcgill.ca>
2024-05-24 15:59:53 -0400

    added test fours

--- tests/test_fours_box.py
+++ tests/test_fours_box.py
@@ -2,3 +2,8 @@
 
 import numpy as np
 import yahtzee.fours_box
+
+def test_fours():
+    """Test fours"""
+    name, score = yahtzee.score_dice(np.array([4, 1, 4, 2, 5]))
+    assert name == "Fours" and score == 8
