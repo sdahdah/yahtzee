64bbdf00b6ef180a8feec6a32326dceb921aad55
junha-yoo <junha.yoo@mail.mcgill.ca>
2024-05-24 16:08:55 -0400

    added three_of_a_kind and test_three_of_a_kind

--- tests/test_three_of_a_kind_box.py
+++ tests/test_three_of_a_kind_box.py
@@ -2,3 +2,14 @@
 
 import numpy as np
 import yahtzee.three_of_a_kind_box
+
+def test_three_of_a_kind():
+    """Test three of a kind."""
+    dice = np.array([1, 2, 3, 4, 5])
+    assert yahtzee.three_of_a_kind_box.three_of_a_kind(dice) == 0
+
+    dice = np.array([1, 1, 1, 4, 5])
+    assert yahtzee.three_of_a_kind_box.three_of_a_kind(dice) == 12
+
+    dice = np.array([1, 1, 1, 1, 1])
+    assert yahtzee.three_of_a_kind_box.three_of_a_kind(dice) == 5
